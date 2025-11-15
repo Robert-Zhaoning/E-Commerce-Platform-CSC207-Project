6dbd42716187ba0674a5ddeb31a756845bc6bf82
Eric Xu <eric.x3254@gmail.com>
2025-11-15 18:42:37 -0500

    minor changes

--- src/main/java/use_case/add_to_cart/AddToCartInteractor.java
+++ src/main/java/use_case/add_to_cart/AddToCartInteractor.java
@@ -1,7 +1,11 @@
 package use_case.add_to_cart;
 
+import entity.User;
+
 public class AddToCartInteractor implements AddToCartInputBoundary {
     private final AddToCartUserDataAccessInterface userDataAccessObject;
+    private final AddToCartOutputBoundary userPresenter;
+
 
     @Override
     public void execute(AddToCartInputData addToCartInputData) {}
