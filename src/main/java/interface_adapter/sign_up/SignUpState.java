479ce49e9a90e8e2fd6fe316078a896dd7eee0bd
Reza Homayounmehr <rezahm2004@gmail.com>
2025-11-15 22:52:08 -0500

    The second (local) version of the SignUpState

--- src/main/java/interface_adapter/sign_up/SignUpState.java
+++ src/main/java/interface_adapter/sign_up/SignUpState.java
@@ -1,5 +1,24 @@
 package interface_adapter.sign_up;
 
-public class SignUpState {
+import entity.User;
 
+public class SignUpState {
+    private User user;
+    private String error;
+
+    public void setSuccess(User user){
+        this.user = user;
+    }
+
+    public void setFailure(String error){
+        this.error = error;
+    }
+
+    public User getSuccess(){
+        return user;
+    }
+
+    public String getFailure(){
+        return error;
+    }
 }
