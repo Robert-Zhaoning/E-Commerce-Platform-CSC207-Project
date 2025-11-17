568b0ea220d032f9fc48440041a519ef6ff445d4
Reza Homayounmehr <rezahm2004@gmail.com>
2025-11-16 21:38:21 -0500

    The first (remote) version of the SignUpState

--- src/main/java/interface_adapter/sign_up/SignUpState.java
+++ src/main/java/interface_adapter/sign_up/SignUpState.java
@@ -2,6 +2,10 @@ package interface_adapter.sign_up;
 
 import entity.User;
 
+/**
+ * This class contains the data needed for state of the signup use case.
+ * SignUpState contains a user or an error.
+ * */
 public class SignUpState {
     private User user;
     private String error;
