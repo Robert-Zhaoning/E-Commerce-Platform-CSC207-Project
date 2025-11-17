9bb6f95abaf2f04d7916629e0af119b354d61ce7
Reza Homayounmehr <rezahm2004@gmail.com>
2025-11-16 21:35:39 -0500

    The first (remote) version of the SignUpViewModel

--- src/main/java/interface_adapter/sign_up/SignUpViewModel.java
+++ src/main/java/interface_adapter/sign_up/SignUpViewModel.java
@@ -2,7 +2,14 @@ package interface_adapter.sign_up;
 
 import interface_adapter.ViewModel;
 
+/**
+ * This class extends the ViewModel and initialise it for the signup use case
+ * */
 public class SignUpViewModel extends ViewModel {
+
+    /**
+     * Creates a SignUpViewModel object for the updating the SignUpView
+     * */
     public SignUpViewModel(){
         super("Sign up");
         this.setState(new SignUpState());
